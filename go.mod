d91cfd49bcceea9620b003461c9751f364fa4db4
Vishesh Handa <me@vhanda.in>
2022-06-21 21:00:40 +0200

    Finish the extractor
    Way way too much code duplication. I wish GoLang was more functional.

--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/vhanda/amazon-email-parser
 go 1.18
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/davecgh/go-spew v1.1.0
+	github.com/jhillyerd/enmime v0.9.4
+	github.com/shopspring/decimal v1.3.1
+)
+
+require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/cention-sany/utf7 v0.0.0-20170124080048-26cad61bd60a // indirect
 	github.com/gogs/chardet v0.0.0-20191104214054-4b6791f73a28 // indirect
 	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
-	github.com/jhillyerd/enmime v0.9.4 // indirect
 	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -8,8 +8,18 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/jhillyerd/enmime"
+	"github.com/shopspring/decimal"
 )
+
+type orderInfo struct {
+	Name     string
+	Merchant string
+	ID       string
+	currency string
+	price    decimal.Decimal
+}
 
 type dumper struct {
 	errOut, stdOut io.Writer
@@ -28,6 +38,102 @@ func newDefaultDumper() *dumper {
 func main() {
 	d := newDefaultDumper()
 	d.exit(d.dump(os.Args))
+}
+
+func extractNames(doc *goquery.Document) []string {
+	names := []string{}
+
+	i := 0
+	selector := "table#itemDetails a"
+	doc.Find(selector).Each(func(_ int, s *goquery.Selection) {
+		title := s.Text()
+		title = strings.TrimSpace(title)
+		if len(title) == 0 {
+			return
+		}
+		if title == "Gestionado por Amazon" {
+			return
+		}
+
+		if (i+1)%2 == 1 {
+			names = append(names, title)
+		}
+		i++
+	})
+
+	return names
+}
+
+func extractMerchants(doc *goquery.Document) []string {
+	names := []string{}
+
+	i := 0
+	selector := "table#itemDetails a"
+	doc.Find(selector).Each(func(_ int, s *goquery.Selection) {
+		title := s.Text()
+		title = strings.TrimSpace(title)
+		if len(title) == 0 {
+			return
+		}
+		if title == "Gestionado por Amazon" {
+			return
+		}
+
+		if (i+1)%2 == 0 {
+			names = append(names, title)
+		}
+		i++
+	})
+
+	return names
+}
+
+func filter(inputs []string, notMatch []string) []string {
+	outputs := []string{}
+
+	for _, i := range inputs {
+		ignore := false
+		for _, not := range notMatch {
+			if i == not {
+				ignore = true
+				break
+			}
+		}
+
+		if !ignore {
+			outputs = append(outputs, i)
+		}
+	}
+	return outputs
+}
+
+func extractCssSelector(doc *goquery.Document, selector string) []string {
+	arr := []string{}
+
+	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		title = strings.TrimSpace(title)
+		if len(title) == 0 {
+			return
+		}
+		arr = append(arr, title)
+	})
+
+	return arr
+}
+
+func extractCurrencyAndPrice(input string) (string, decimal.Decimal, error) {
+	parts := strings.Split(input, " ")
+	num := parts[1]
+	num = strings.ReplaceAll(num, ".", "")
+	num = strings.Replace(num, ",", ".", 1)
+
+	d, err := decimal.NewFromString(num)
+	if err != nil {
+		return "", decimal.Decimal{}, err
+	}
+
+	return parts[0], d, nil
 }
 
 func (d *dumper) dump(args []string) int {
@@ -53,18 +159,31 @@ func (d *dumper) dump(args []string) int {
 		log.Fatal(err)
 	}
 
-	selector := "table#itemDetails a"
-	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
-		title := s.Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	names := extractNames(doc)
+	merchants := extractMerchants(doc)
+	ids := extractCssSelector(doc, "#orderDetails a")
+	prices := filter(extractCssSelector(doc, "td.price"), []string{"Mastercard", "Visa"})
 
-	orderNumberSelector := "#orderDetails a"
-	doc.Find(orderNumberSelector).Each(func(i int, s *goquery.Selection) {
-		fmt.Printf("Order: %s\n", s.Text())
-		// fmt.Printf("Date: %s\n", s.Find("span").Text())
-	})
+	orders := []orderInfo{}
+	for i := range names {
+		pStr := prices[i]
+		currency, price, err := extractCurrencyAndPrice(pStr)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		o := orderInfo{
+			Name:     names[i],
+			Merchant: merchants[i],
+			ID:       ids[i],
+			currency: currency,
+			price:    price,
+		}
+
+		orders = append(orders, o)
+	}
+
+	spew.Dump(orders)
+
 	return 0
 }
-
-//
